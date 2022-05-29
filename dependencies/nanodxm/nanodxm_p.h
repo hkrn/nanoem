hkrn/nanoem

commit 2fadbbb2b48c3893b609eb8821a6dca2809d997f

[emapp] implement nanodxm/nanomqo fuzzing test program (#135)
* [nanomqo] implement fuzzer test program

* [nanodxm] reimplement fuzzer test program to latest

* [nanoem] rework fuzzing test program

The commit fixes memory leaks detected by its fuzz test program.

* [cmake] fix build failure of fuzzing test program

* [emapp] fix a crash bug when empty PFM is loading

dependencies/nanodxm/nanodxm_p.h
@@ -98,7 +98,7 @@ nanodxmFree(void *ptr, const char *filename, int line)
 #define nanodxm_realloc(ptr, size) nanodxmRealloc((ptr), (size), NULL, 0)
 #define nanodxm_calloc(length, size) nanodxmCalloc((length), (size), NULL, 0)
 #define nanodxm_free(ptr) nanodxmFree((ptr), NULL, 0)
-#endif
+#endif /* NANODXM_ENABLE_DEBUG_ALLOCATOR */
 
 #define NANODXM_CHECK_CLOSE_BRACE(ptr, err) \
     for (;;) {                              \
@@ -326,36 +326,69 @@ nanodxmBufferEndChunk(nanodxm_buffer_t *buffer, nanodxm_parser_state_t *state)
 }
 
 NANODXM_DECL_INLINE static void
+nanodxmBufferReadStackString(nanodxm_buffer_t *buffer, char *stack, nanodxm_rsize_t size)
+{
+    nanodxm_rsize_t len;
+    size -= 1;
+    if (nanodxmBufferCanReadLength(buffer, size)) {
+        memcpy(stack, nanodxmBufferGetDataPtr(buffer), size);
+        stack[size] = 0;
+    }
+    else if (buffer->length > buffer->offset) {
+        len = buffer->length - buffer->offset;
+        memcpy(stack, nanodxmBufferGetDataPtr(buffer), len);
+    }
+    else {
+        stack[0] = 0;
+    }
+}
+
+NANODXM_DECL_INLINE static void
 nanodxmBufferExtractInt(nanodxm_buffer_t *buffer, int *value)
 {
-    char *ptr, *end;
+    char stack[16], *ptr, *end;
     nanodxmBufferSkipSpaces(buffer);
-    ptr = (char *) nanodxmBufferGetDataPtr(buffer);
-    *value = strtol(ptr, &end, 10);
-    buffer->offset += end - ptr;
-    nanodxmBufferSkipSpaces(buffer);
+    if ((ptr = (char *) nanodxmBufferGetDataPtr(buffer)) != NULL) {
+        nanodxmBufferReadStackString(buffer, stack, sizeof(stack));
+        *value = strtol(stack, &end, 10);
+        buffer->offset += end - ptr;
+        nanodxmBufferSkipSpaces(buffer);
+    }
+    else {
+        *value = 0;
+    }
 }
 
 NANODXM_DECL_INLINE static void
 nanodxmBufferExtractSize(nanodxm_buffer_t *buffer, nanodxm_rsize_t *value)
 {
-    char *ptr, *end;
+    char stack[16], *ptr, *end;
     nanodxmBufferSkipSpaces(buffer);
-    ptr = (char *) nanodxmBufferGetDataPtr(buffer);
-    *value = (nanodxm_rsize_t) strtol(ptr, &end, 10);
-    buffer->offset += end - ptr;
-    nanodxmBufferSkipSpaces(buffer);
+    if ((ptr = (char *) nanodxmBufferGetDataPtr(buffer)) != NULL) {
+        nanodxmBufferReadStackString(buffer, stack, sizeof(stack));
+        *value = (nanodxm_rsize_t) strtol(ptr, &end, 10);
+        buffer->offset += end - ptr;
+        nanodxmBufferSkipSpaces(buffer);
+    }
+    else {
+        *value = 0;
+    }
 }
 
 NANODXM_DECL_INLINE static void
 nanodxmBufferExtractFloat(nanodxm_buffer_t *buffer, float *value)
 {
-    char *ptr, *end;
+    char stack[16], *ptr, *end;
     nanodxmBufferSkipSpaces(buffer);
-    ptr = (char *) nanodxmBufferGetDataPtr(buffer);
-    *value = (float) strtod(ptr, &end);
-    buffer->offset += end - ptr;
-    nanodxmBufferSkipSpaces(buffer);
+    if ((ptr = (char *) nanodxmBufferGetDataPtr(buffer)) != NULL) {
+        nanodxmBufferReadStackString(buffer, stack, sizeof(stack));
+        *value = (float) strtod(stack, &end);
+        buffer->offset += end - ptr;
+        nanodxmBufferSkipSpaces(buffer);
+    }
+    else {
+        *value = 0;
+    }
 }
 
 #endif /* NANODXM_PRIVATE_H_ */

dependencies/nanodxm/CMakeLists.txt
@@ -4,7 +4,6 @@ set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/nanoem.cmake)
 option(${PROJECT_NAME_PREFIX}_ENABLE_TEST_RUNNER "Enable building test runner option." OFF)
-option(${PROJECT_NAME_PREFIX}_ENABLE_BGFX "Enable building BGFX renderer option." OFF)
 option(${PROJECT_NAME_PREFIX}_ENABLE_FUZZER "Enable building fuzzing program with libFuzzer." OFF)
 
 include_directories(${CMAKE_SOURCE_DIR}/deps)
@@ -21,27 +20,9 @@ if(${PROJECT_NAME_PREFIX}_ENABLE_TEST_RUNNER)
   endif()
 endif()
 
-if(${PROJECT_NAME_PREFIX}_ENABLE_BGFX)
-  find_bgfx()
-  find_path(GLM_INCLUDE_DIR glm/glm.hpp PATHS $ENV{GLM_LOCATION} NO_DEFAULT_PATH)
-  mark_as_advanced(GLM_INCLUDE_DIR)
-  aux_source_directory(${BGFX_EXAMPLE_COMMON_DIR}/nanovg BGFX_NANOVG_SOURCES)
-  aux_source_directory(${BGFX_EXAMPLE_COMMON_DIR}/imgui BGFX_IMGUI_SOURCES)
-  add_library(bgfxaux ${${PROJECT_NAME_PREFIX}_LIBRARY_TYPE} ${BGFX_IMGUI_SOURCES} ${BGFX_NANOVG_SOURCES} ${CMAKE_CURRENT_SOURCE_DIR}/deps/parson/parson.c)
-  add_executable(${PROJECT_NAME}_bgfx ${CMAKE_CURRENT_SOURCE_DIR}/bgfx.cc)
-  include_directories(${BX_INCLUDE_DIR} ${BGFX_INCLUDE_DIR} ${BGFX_EXAMPLE_COMMON_DIR} ${BGFX_THIRDPARTY_DIR} ${BX_COMPAT_INCLUDE_PATH} ${GLM_INCLUDE_DIR} ${STB_INSTALL_PREFIX})
-  target_link_libraries(${PROJECT_NAME}_bgfx ${PROJECT_NAME} bgfxaux)
-  link_bgfx(${PROJECT_NAME}_bgfx)
-  link_sdl2(${PROJECT_NAME}_bgfx)
-  add_definitions(-DBGFX_SDL2)
-endif()
-
 if(${PROJECT_NAME_PREFIX}_ENABLE_FUZZER)
-  find_library(LIBFUZZER_LIBRARY Fuzzer PATH_SUFFIXES lib PATHS /usr/lib/llvm-4.0)
-  mark_as_advanced(LIBFUZZER_LIBRARY)
   add_executable(${PROJECT_NAME}_fuzz fuzz.cc)
-  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY COMPILE_OPTIONS -fsanitize=address -fno-omit-frame-pointer -fsanitize-coverage=trace-pc-guard)
-  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY LINK_FLAGS "-fsanitize=address -fno-omit-frame-pointer -fsanitize-coverage=trace-pc-guard")
-  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY LINK_LIBRARIES ${PROJECT_NAME} ${LIBFUZZER_LIBRARY})
+  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY COMPILE_OPTIONS -fsanitize=fuzzer,address)
+  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY LINK_FLAGS -fsanitize=fuzzer,address)
+  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY LINK_LIBRARIES ${PROJECT_NAME})
 endif()
-

dependencies/nanodxm/fuzz.cc
@@ -4,18 +4,17 @@
    This file is licensed under MIT license. for more details, see LICENSE.txt.
  */
 
-#include <stdint.h>
 #include "nanodxm.h"
+
+#include <stdint.h>
 
 extern "C" int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    nanodxm_status_t status = NANODXM_STATUS_SUCCESS;
     nanodxm_document_t *document = nanodxmDocumentCreate();
     nanodxm_buffer_t *buffer = nanodxmBufferCreate(data, size);
-    status = nanodxmDocumentParse(document, buffer);
+    nanodxmDocumentParse(document, buffer);
     nanodxmBufferDestroy(buffer);
     nanodxmDocumentDestroy(document);
     return 0;
 }
-

dependencies/nanodxm/nanodxm.c
@@ -114,6 +114,9 @@ nanodxmMaterialCreate(void)
     nanodxm_material_t *material;
     material = (nanodxm_material_t *) nanodxm_calloc(1, sizeof(*material));
     if (nanodxm_is_not_null(material)) {
+        material->diffuse = __nanodxm_color_white;
+        material->emissive.a = 1.0f;
+        material->specular.a = 1.0f;
     }
     return material;
 }
@@ -271,15 +274,17 @@ nanodxmDocumentParseMeshNormals(nanodxm_document_t *document, nanodxm_buffer_t *
         face = &document->normal_faces[i];
         nanodxmBufferExtractSize(buffer, &face->num_indices);
         nanodxmBufferSkipSeparator(buffer);
-        face->indices = (int *) nanodxm_calloc(face->num_indices, sizeof(*face->indices));
-        if (nanodxm_is_not_null(face->indices)) {
-            for (j = 0; j < face->num_indices; j++) {
-                nanodxmBufferExtractInt(buffer, &face->indices[j]);
-                nanodxmBufferSkipSeparator(buffer);
+        if (face->num_indices > 0) {
+            face->indices = (int *) nanodxm_calloc(face->num_indices, sizeof(*face->indices));
+            if (nanodxm_is_not_null(face->indices)) {
+                for (j = 0; j < face->num_indices; j++) {
+                    nanodxmBufferExtractInt(buffer, &face->indices[j]);
+                    nanodxmBufferSkipSeparator(buffer);
+                }
             }
-        }
-        else {
-            face->num_indices = 0;
+            else {
+                face->num_indices = 0;
+            }
         }
     }
     nanodxmBufferSkipSpaces(buffer);
@@ -318,6 +323,7 @@ nanodxmDocumentParseMeshVertexColors(nanodxm_document_t *document, nanodxm_buffe
     state->depth++;
     if (document->vertex_colors) {
         nanodxm_free(document->vertex_colors);
+        document->vertex_colors = NULL;
     }
     document->vertex_colors = (nanodxm_color_t *) nanodxm_calloc(document->num_vertex_colors, sizeof(*document->vertex_colors));
     length = document->num_vertex_colors;
@@ -347,7 +353,7 @@ nanodxmDocumentParseMesh(nanodxm_document_t *document, nanodxm_buffer_t *buffer,
     nanodxmBufferExtractSize(buffer, &document->num_vertices);
     nanodxmBufferSkipSeparator(buffer);
     state->depth++;
-    if (document->vertices) {
+    if (!nanodxm_is_null(document->vertices)) {
         nanodxm_free(document->vertices);
     }
     document->vertices = (nanodxm_vector3_t *) nanodxm_calloc(document->num_vertices, sizeof(*document->vertices));
@@ -362,7 +368,7 @@ nanodxmDocumentParseMesh(nanodxm_document_t *document, nanodxm_buffer_t *buffer,
         nanodxmBufferSkipSeparator(buffer);
     }
     nanodxmBufferSkipSpaces(buffer);
-    if (document->vertices) {
+    if (!nanodxm_is_null(document->vertex_faces)) {
         length = document->num_vertex_faces;
         for (i = 0; i < length; i++) {
             nanodxm_free(document->vertex_faces[length - i - 1].indices);
@@ -377,15 +383,17 @@ nanodxmDocumentParseMesh(nanodxm_document_t *document, nanodxm_buffer_t *buffer,
         face = &document->vertex_faces[i];
         nanodxmBufferExtractSize(buffer, &face->num_indices);
         nanodxmBufferSkipSeparator(buffer);
-        face->indices = (int *) nanodxm_calloc(face->num_indices, sizeof(*face->indices));
-        if (nanodxm_is_not_null(face->indices)) {
-            for (j = 0; j < face->num_indices; j++) {
-                nanodxmBufferExtractInt(buffer, &face->indices[j]);
-                nanodxmBufferSkipSeparator(buffer);
+        if (face->num_indices > 0) {
+            face->indices = (int *) nanodxm_calloc(face->num_indices, sizeof(*face->indices));
+            if (nanodxm_is_not_null(face->indices)) {
+                for (j = 0; j < face->num_indices; j++) {
+                    nanodxmBufferExtractInt(buffer, &face->indices[j]);
+                    nanodxmBufferSkipSeparator(buffer);
+                }
             }
-        }
-        else {
-            face->num_indices = 0;
+            else {
+                face->num_indices = 0;
+            }
         }
     }
     nanodxmBufferSkipSpaces(buffer);
@@ -412,7 +420,6 @@ nanodxmDocumentParseMaterial(nanodxm_document_t *document, nanodxm_buffer_t *buf
         nanodxmBufferExtractFloat(buffer, &material->shininess);
         nanodxmBufferSkipSeparator(buffer);
         /* specular */
-        material->specular.a = 1.0f;
         nanodxmBufferExtractFloat(buffer, &material->specular.r);
         nanodxmBufferSkipSeparator(buffer);
         nanodxmBufferExtractFloat(buffer, &material->specular.g);
@@ -420,7 +427,6 @@ nanodxmDocumentParseMaterial(nanodxm_document_t *document, nanodxm_buffer_t *buf
         nanodxmBufferExtractFloat(buffer, &material->specular.b);
         nanodxmBufferSkipSeparator(buffer);
         /* emissive */
-        material->emissive.a = 1.0f;
         nanodxmBufferExtractFloat(buffer, &material->emissive.r);
         nanodxmBufferSkipSeparator(buffer);
         nanodxmBufferExtractFloat(buffer, &material->emissive.g);
@@ -629,7 +635,6 @@ nanodxmDocumentParseASCII(nanodxm_document_t *document, nanodxm_buffer_t *buffer
     }
     if (kv_size(document->materials) == 0) {
         material = nanodxmMaterialCreate();
-        material->diffuse = __nanodxm_color_white;
         kv_push(nanodxm_material_t *, document->materials, material);
     }
     return NANODXM_STATUS_SUCCESS;
@@ -816,7 +821,7 @@ nanodxmDocumentDestroy(nanodxm_document_t *document)
     if (nanodxm_is_not_null(document)) {
         length = kv_size(document->materials);
         for (i = 0; i < length; i++) {
-            nanodxmMaterialDestroy(kv_A(document->materials, i));
+            nanodxmMaterialDestroy(kv_A(document->materials, length - i - 1));
         }
         kv_destroy(document->materials);
         if (nanodxm_is_not_null(document->vertices)) {
@@ -844,7 +849,7 @@ nanodxmDocumentDestroy(nanodxm_document_t *document)
         if (nanodxm_is_not_null(document->vertex_faces)) {
             length = document->num_vertex_faces;
             for (i = 0; i < length; i++) {
-                nanodxm_free(document->vertex_faces[length- i - 1].indices);
+                nanodxm_free(document->vertex_faces[length - i - 1].indices);
             }
             nanodxm_free(document->vertex_faces);
         }

dependencies/nanomqo/CMakeLists.txt
@@ -2,12 +2,12 @@ project(nanomqo)
 cmake_minimum_required(VERSION 2.8)
 set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/nanoem.cmake)
-option(${PROJECT_NAME_PREFIX}_ENABLE_TEST_RUNNER "Enable building test runner option." ON)
+option(${PROJECT_NAME}_ENABLE_TEST_RUNNER "Enable building test runner option." ON)
+option(${PROJECT_NAME}_ENABLE_FUZZER "Enable building fuzzing program with libFuzzer." OFF)
 
 include_directories(${CMAKE_SOURCE_DIR}/deps)
-add_library(${PROJECT_NAME} ${${PROJECT_NAME_PREFIX}_LIBRARY_TYPE} ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.c ${PROJECT_NAME}_p.h)
-set_target_properties(${PROJECT_NAME} PROPERTIES DEFINE_SYMBOL "${PROJECT_NAME_PREFIX}_DLL_EXPORTS")
+add_library(${PROJECT_NAME} ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.c ${PROJECT_NAME}_p.h)
+set_target_properties(${PROJECT_NAME} PROPERTIES DEFINE_SYMBOL "${PROJECT_NAME}_DLL_EXPORTS")
 
 if(${PROJECT_NAME_PREFIX}_ENABLE_TEST_RUNNER)
   add_executable(${PROJECT_NAME}_test ${CMAKE_CURRENT_SOURCE_DIR}/test.c)
@@ -17,4 +17,11 @@ if(${PROJECT_NAME_PREFIX}_ENABLE_TEST_RUNNER)
     mark_as_advanced(FOUNDATION_FRAMEWORK)
     target_link_libraries(${PROJECT_NAME}_test ${FOUNDATION_FRAMEWORK})
   endif()
+endif()
+
+if(${PROJECT_NAME}_ENABLE_FUZZER)
+  add_executable(${PROJECT_NAME}_fuzz fuzz.cc)
+  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY COMPILE_OPTIONS -fsanitize=fuzzer,address)
+  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY LINK_FLAGS -fsanitize=fuzzer,address)
+  set_property(TARGET ${PROJECT_NAME}_fuzz APPEND PROPERTY LINK_LIBRARIES ${PROJECT_NAME})
 endif()

dependencies/nanomqo/fuzz.cc
@@ -1,0 +1,21 @@
+/*
+   Copyright (c) 2015-2021 hkrn All rights reserved
+
+   This file is licensed under MIT license. for more details, see LICENSE.txt.
+ */
+
+#include "nanomqo.h"
+
+#include <stdint.h>
+#include <stdio.h>
+
+extern "C" int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    nanomqo_document_t *document = nanomqoDocumentCreate();
+    nanomqo_buffer_t *buffer = nanomqoBufferCreate(data, size);
+    nanomqoDocumentParse(document, buffer);
+    nanomqoBufferDestroy(buffer);
+    nanomqoDocumentDestroy(document);
+    return 0;
+}

dependencies/nanomqo/nanomqo_p.h
@@ -269,6 +269,20 @@ nanomqoBufferGetChar(const nanomqo_buffer_t *buffer)
     return nanomqo_likely(buffer->offset < buffer->length) ? *(buffer->data + buffer->offset) : nanomqo_eos;
 }
 
+NANOMQO_DECL_INLINE static void
+nanomqoBufferAdvanceChar(nanomqo_buffer_t *buffer)
+{
+    buffer->offset++;
+    buffer->columns++;
+}
+
+NANOMQO_DECL_INLINE static nanomqo_uint8_t
+nanomqoBufferReadNextChar(nanomqo_buffer_t *buffer)
+{
+    nanomqoBufferAdvanceChar(buffer);
+    return nanomqoBufferGetChar(buffer);
+}
+
 NANOMQO_DECL_INLINE static nanomqo_bool_t
 nanomqoBufferTestString(const nanomqo_buffer_t *buffer, const char *const text)
 {
@@ -283,9 +297,7 @@ nanomqoBufferSkipSpaces(nanomqo_buffer_t *buffer)
 {
     nanomqo_uint8_t c = nanomqoBufferGetChar(buffer);
     while (c != nanomqo_eos && isspace(c)) {
-        buffer->offset++;
-        buffer->columns++;
-        c = nanomqoBufferGetChar(buffer);
+        c = nanomqoBufferReadNextChar(buffer);
     }
 }
 
@@ -294,7 +306,7 @@ nanomqoBufferSkipLine(nanomqo_buffer_t *buffer)
 {
     nanomqo_uint8_t c1 = nanomqoBufferGetChar(buffer), c2;
     while (c1 != nanomqo_eos) {
-        buffer->offset++;
+        nanomqoBufferAdvanceChar(buffer);
         if (c1 == '\n') {
             buffer->rows++;
             buffer->columns = 0;
@@ -302,7 +314,7 @@ nanomqoBufferSkipLine(nanomqo_buffer_t *buffer)
         }
         c2 = nanomqoBufferGetChar(buffer);
         if (c1 == '\r' && c2 == '\n') {
-            buffer->offset++;
+            nanomqoBufferAdvanceChar(buffer);
             buffer->rows++;
             buffer->columns = 0;
             break;
@@ -317,8 +329,7 @@ nanomqoBufferSkipUntilEndChunk(nanomqo_buffer_t *buffer)
     nanomqo_uint8_t c1 = nanomqoBufferGetChar(buffer), c2;
     int depth = 0;
     while (c1 != nanomqo_eos && depth >= 0) {
-        buffer->offset++;
-        buffer->columns++;
+        nanomqoBufferAdvanceChar(buffer);
         if (c1 == '{') {
             buffer->depth++;
             depth++;

emapp/CMakeLists.txt
@@ -166,7 +166,7 @@ function(nanoem_emapp_build_emapp suffix)
                  IMGUI_USER_CONFIG="emapp/private/imgui/config.h")
   endif()
   if(NANOEM_ENABLE_IMGUI_FILE_DIALOG)
-    set_property(TARGET ${_emapp_name} embundle APPEND PROPERTY COMPILE_DEFINITIONS NANOEM_ENABLE_IMGUI_FILE_DIALOG)
+    set_property(TARGET ${_emapp_name} ${_embundle_name} APPEND PROPERTY COMPILE_DEFINITIONS NANOEM_ENABLE_IMGUI_FILE_DIALOG)
   endif()
   get_property(_include_directories TARGET nanoem PROPERTY INCLUDE_DIRECTORIES)
   target_include_directories(${_emapp_name} PRIVATE
@@ -325,6 +325,6 @@ foreach(item ${NANOEM_EXECUTABLES})
   nanoem_emapp_link_executable(${item})
 endforeach()
 
-if(OFF)
+if(NANOEM_ENABLE_FUZZER)
   add_subdirectory(fuzz)
 endif()

commit 9d86e53a0c28c7b39619ecfceb7012404cf69624

[nanodxm] fixed a bug cannot read int/float properly
This commit fixed a bug of reading int/float due to incorrect
pointer calculation caused in 2fadbbb2b48c3893b609eb8821a6dca2809d997f.

dependencies/nanodxm/nanodxm_p.h
@@ -346,12 +346,12 @@ nanodxmBufferReadStackString(nanodxm_buffer_t *buffer, char *stack, nanodxm_rsiz
 NANODXM_DECL_INLINE static void
 nanodxmBufferExtractInt(nanodxm_buffer_t *buffer, int *value)
 {
-    char stack[16], *ptr, *end;
+    char stack[16], *ptr, *end = NULL;
     nanodxmBufferSkipSpaces(buffer);
     if ((ptr = (char *) nanodxmBufferGetDataPtr(buffer)) != NULL) {
         nanodxmBufferReadStackString(buffer, stack, sizeof(stack));
         *value = strtol(stack, &end, 10);
-        buffer->offset += end - ptr;
+        buffer->offset += end - stack;
         nanodxmBufferSkipSpaces(buffer);
     }
     else {
@@ -362,12 +362,12 @@ nanodxmBufferExtractInt(nanodxm_buffer_t *buffer, int *value)
 NANODXM_DECL_INLINE static void
 nanodxmBufferExtractSize(nanodxm_buffer_t *buffer, nanodxm_rsize_t *value)
 {
-    char stack[16], *ptr, *end;
+    char stack[16], *ptr, *end = NULL;
     nanodxmBufferSkipSpaces(buffer);
     if ((ptr = (char *) nanodxmBufferGetDataPtr(buffer)) != NULL) {
         nanodxmBufferReadStackString(buffer, stack, sizeof(stack));
-        *value = (nanodxm_rsize_t) strtol(ptr, &end, 10);
-        buffer->offset += end - ptr;
+        *value = (nanodxm_rsize_t) strtol(stack, &end, 10);
+        buffer->offset += end - stack;
         nanodxmBufferSkipSpaces(buffer);
     }
     else {
@@ -378,12 +378,12 @@ nanodxmBufferExtractSize(nanodxm_buffer_t *buffer, nanodxm_rsize_t *value)
 NANODXM_DECL_INLINE static void
 nanodxmBufferExtractFloat(nanodxm_buffer_t *buffer, float *value)
 {
-    char stack[16], *ptr, *end;
+    char stack[16], *ptr, *end = NULL;
     nanodxmBufferSkipSpaces(buffer);
     if ((ptr = (char *) nanodxmBufferGetDataPtr(buffer)) != NULL) {
         nanodxmBufferReadStackString(buffer, stack, sizeof(stack));
         *value = (float) strtod(stack, &end);
-        buffer->offset += end - ptr;
+        buffer->offset += end - stack;
         nanodxmBufferSkipSpaces(buffer);
     }
     else {