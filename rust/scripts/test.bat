bcbb48063734a758422d4ad8559316734ee58b37
hkrn <129939+hkrn@users.noreply.github.com>
2022-06-10 00:04:07 +0900

    [rust] add batch running test scripts

---
+++ rust/scripts/test.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rustup target add wasm32-wasi
+cargo install cargo-deny
+cargo deny check
+cargo build --release --package plugin_wasm_test_model_minimum --target wasm32-wasi
+cargo build --release --package plugin_wasm_test_motion_minimum --target wasm32-wasi
+cargo build --release --package plugin_wasm_test_model_full --target wasm32-wasi
+cargo build --release --package plugin_wasm_test_motion_full --target wasm32-wasi
+cargo test --release
