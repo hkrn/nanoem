17bae68bb09980515dd53995c380fb8a024edf0b
hkrn <129939+hkrn@users.noreply.github.com>
2023-04-23 21:44:10 +0900

    [rust] fix `rust/scripts/build.bat` to be runnable

--- rust/scripts/build.bat
+++ rust/scripts/build.bat
@@ -6,10 +6,10 @@ set packages=plugin_wasm_test_model_minimum plugin_wasm_test_motion_minimum plug
 rustup target add wasm32-wasi
 cargo install cargo-deny
 cargo deny check
-for %%profile in %profiles%
-  for %%package in %packages%
-    cargo build --profile %%profile --package %%package --target wasm32-wasi
-  done
-done
+for %%i in (%profiles%) do (
+  for %%j in (%packages%) do (
+    cargo build --profile %%i --package %%j --target wasm32-wasi
+  )
+)
 cargo build --profile release-lto
 cargo test --profile release-lto
