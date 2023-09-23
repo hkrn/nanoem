57f69a83feb0753b4dd9b364ff5a1202ce764ceb
hkrn <129939+hkrn@users.noreply.github.com>
2023-09-23 19:30:55 +0900

    [rust] add capability to watch and reload WASM plugins (#370)

--- rust/plugin_wasm/src/model/plugin.rs
+++ rust/plugin_wasm/src/model/plugin.rs
@@ -4,12 +4,17 @@
   This file is part of emapp component and it's licensed under Mozilla Public License. see LICENSE.md for more details.
 */
 
-use std::{ffi::CString, path::Path};
+use std::{
+    ffi::CString,
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex},
+};
 
 use anyhow::Result;
+use notify::Watcher;
 use walkdir::WalkDir;
 use wasmtime::{AsContextMut, Engine, Instance, Linker, Module};
-use wasmtime_wasi::WasiCtxBuilder;
+use wasmtime_wasi::{WasiCtx, WasiCtxBuilder};
 
 use crate::{
     inner_count_all_functions, inner_create_opaque, inner_destroy_opaque, inner_execute,
@@ -83,19 +88,25 @@ fn validate_plugin(instance: &Instance, mut store: impl AsContextMut) -> Result<
 pub struct ModelIOPlugin {
     instance: Instance,
     store: Store,
+    path: PathBuf,
     opaque: Option<OpaquePtr>,
 }
 
 impl ModelIOPlugin {
-    pub fn new(engine: &Engine, bytes: &[u8], mut store: Store) -> Result<Self> {
-        let module = Module::new(engine, bytes)?;
-        let mut linker = Linker::new(engine);
-        wasmtime_wasi::add_to_linker(&mut linker, |ctx| ctx)?;
+    pub fn new(
+        linker: &Linker<WasiCtx>,
+        path: &Path,
+        bytes: &[u8],
+        mut store: Store,
+    ) -> Result<Self> {
+        let module = Module::new(linker.engine(), bytes)?;
         let instance = linker.instantiate(store.as_context_mut(), &module)?;
         validate_plugin(&instance, store.as_context_mut())?;
+        let path = path.to_path_buf();
         Ok(Self {
             instance,
             store,
+            path,
             opaque: None,
         })
     }
@@ -372,10 +383,13 @@ impl ModelIOPlugin {
             &mut self.store,
         )
     }
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
 }
 
 pub struct ModelIOPluginController {
-    plugins: Vec<ModelIOPlugin>,
+    plugins: Arc<Mutex<Vec<ModelIOPlugin>>>,
     function_indices: Vec<(usize, i32, CString)>,
     plugin_index: Option<usize>,
     failure_reason: Option<String>,
@@ -383,7 +397,7 @@ pub struct ModelIOPluginController {
 }
 
 impl ModelIOPluginController {
-    pub fn new(plugins: Vec<ModelIOPlugin>) -> Self {
+    pub fn new(plugins: Arc<Mutex<Vec<ModelIOPlugin>>>) -> Self {
         let function_indices = vec![];
         Self {
             plugins,
@@ -395,10 +409,55 @@ impl ModelIOPluginController {
     }
     pub fn from_path<F>(path: &Path, cb: F) -> Result<Self>
     where
-        F: Fn(&mut WasiCtxBuilder),
+        F: Fn(&mut WasiCtxBuilder) + Copy + std::marker::Sync + std::marker::Send + 'static,
     {
-        let mut plugins = vec![];
         let engine = Engine::default();
+        let plugins = Arc::new(Mutex::new(vec![]));
+        let plugins_inner = Arc::clone(&plugins);
+        let mut linker = Linker::new(&engine);
+        let linker_inner = linker.clone();
+        wasmtime_wasi::add_to_linker(&mut linker, |ctx| ctx)?;
+        let event_handler = move |res: notify::Result<notify::Event>| match res {
+            Ok(ev) => match ev.kind {
+                notify::EventKind::Modify(_) => {
+                    for path in ev.paths.iter() {
+                        if let Some(plugin_mut) = plugins_inner
+                            .lock()
+                            .unwrap()
+                            .iter_mut()
+                            .find(|plugin: &&mut ModelIOPlugin| plugin.path() == path)
+                        {
+                            let bytes = std::fs::read(path).unwrap();
+                            let mut builder = WasiCtxBuilder::new();
+                            cb(&mut builder);
+                            let data = builder.build();
+                            let store = Store::new(linker_inner.engine(), data);
+                            match ModelIOPlugin::new(&linker_inner, path, &bytes, store) {
+                                Ok(plugin) => {
+                                    *plugin_mut = plugin;
+                                }
+                                Err(err) => {
+                                    tracing::warn!(
+                                        error = %err,
+                                        path = ?path,
+                                        "Cannot reload model WASM plugin",
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+                notify::EventKind::Remove(_) => {
+                    plugins_inner
+                        .lock()
+                        .unwrap()
+                        .retain(|plugin| !ev.paths.contains(&plugin.path));
+                }
+                _ => {}
+            },
+            Err(e) => tracing::warn!(error = ?e, "Catched an watch error"),
+        };
+        let mut watcher = notify::recommended_watcher(event_handler)?;
         for entry in WalkDir::new(path.parent().unwrap()) {
             let entry = entry?;
             let filename = entry.file_name().to_str();
@@ -407,10 +466,12 @@ impl ModelIOPluginController {
                 let mut builder = WasiCtxBuilder::new();
                 cb(&mut builder);
                 let data = builder.build();
-                let store = Store::new(&engine, data);
-                match ModelIOPlugin::new(&engine, &bytes, store) {
+                let store = Store::new(linker.engine(), data);
+                let path = entry.path();
+                match ModelIOPlugin::new(&linker, path, &bytes, store) {
                     Ok(plugin) => {
-                        plugins.push(plugin);
+                        watcher.watch(path, notify::RecursiveMode::NonRecursive)?;
+                        plugins.lock().unwrap().push(plugin);
                         tracing::debug!(filename = filename.unwrap(), "Loaded model WASM plugin");
                     }
                     Err(err) => {
@@ -427,14 +488,15 @@ impl ModelIOPluginController {
     }
     pub fn initialize(&mut self) -> Result<()> {
         self.plugins
+            .lock()
+            .unwrap()
             .iter_mut()
             .try_for_each(|plugin| plugin.initialize())
     }
     pub fn create(&mut self) -> Result<()> {
-        self.plugins
-            .iter_mut()
-            .try_for_each(|plugin| plugin.create())?;
-        for (offset, plugin) in self.plugins.iter_mut().enumerate() {
+        let mut guard = self.plugins.lock().unwrap();
+        guard.iter_mut().try_for_each(|plugin| plugin.create())?;
+        for (offset, plugin) in guard.iter_mut().enumerate() {
             let name = plugin.name()?;
             let version = plugin.version()?;
             for index in 0..plugin.count_all_functions()? {
@@ -448,6 +510,8 @@ impl ModelIOPluginController {
     }
     pub fn set_language(&mut self, value: i32) -> Result<()> {
         self.plugins
+            .lock()
+            .unwrap()
             .iter_mut()
             .try_for_each(|plugin| plugin.set_language(value))
     }
@@ -463,7 +527,7 @@ impl ModelIOPluginController {
     }
     pub fn set_function(&mut self, index: i32) -> Result<i32> {
         if let Some((plugin_index, function_index, _)) = self.function_indices.get(index as usize) {
-            let result = self.plugins[*plugin_index].set_function(*function_index);
+            let result = self.plugins.lock().unwrap()[*plugin_index].set_function(*function_index);
             self.plugin_index = Some(*plugin_index);
             result
         } else {
@@ -471,66 +535,83 @@ impl ModelIOPluginController {
         }
     }
     pub fn set_all_selected_vertex_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?.set_all_selected_vertex_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_vertex_indices(data))
     }
     pub fn set_all_selected_material_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?
-            .set_all_selected_material_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_material_indices(data))
     }
     pub fn set_all_selected_bone_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?.set_all_selected_bone_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_bone_indices(data))
     }
     pub fn set_all_selected_morph_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?.set_all_selected_morph_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_morph_indices(data))
     }
     pub fn set_all_selected_label_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?.set_all_selected_label_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_label_indices(data))
     }
     pub fn set_all_selected_rigid_body_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?
-            .set_all_selected_rigid_body_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_rigid_body_indices(data))
     }
     pub fn set_all_selected_joint_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?.set_all_selected_joint_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_joint_indices(data))
     }
     pub fn set_all_selected_soft_body_indices(&mut self, data: &[i32]) -> Result<()> {
-        self.current_plugin()?
-            .set_all_selected_soft_body_indices(data)
+        self.current_plugin(|plugin| plugin.set_all_selected_soft_body_indices(data))
     }
     pub fn set_audio_description(&mut self, data: &[u8]) -> Result<()> {
-        self.current_plugin()?.set_audio_description(data)
+        self.current_plugin(|plugin| plugin.set_audio_description(data))
     }
     pub fn set_camera_description(&mut self, data: &[u8]) -> Result<()> {
-        self.current_plugin()?.set_camera_description(data)
+        self.current_plugin(|plugin| plugin.set_camera_description(data))
     }
     pub fn set_light_description(&mut self, data: &[u8]) -> Result<()> {
-        self.current_plugin()?.set_light_description(data)
+        self.current_plugin(|plugin| plugin.set_light_description(data))
     }
     pub fn set_audio_data(&mut self, data: &[u8]) -> Result<()> {
-        self.current_plugin()?.set_audio_data(data)
+        self.current_plugin(|plugin| plugin.set_audio_data(data))
     }
     pub fn set_input_model_data(&mut self, bytes: &[u8]) -> Result<()> {
-        self.current_plugin()?.set_input_model_data(bytes)
+        self.current_plugin(|plugin| plugin.set_input_model_data(bytes))
     }
     pub fn execute(&mut self) -> Result<()> {
-        match self.current_plugin()?.execute() {
+        let mut result = Ok(0);
+        self.current_plugin(|plugin| {
+            result = plugin.execute();
+            Ok(())
+        })?;
+        match result {
             Ok(0) => Ok(()),
             Ok(_) => self.set_failure(),
             Err(err) => Err(err),
         }
     }
     pub fn get_output_data(&mut self) -> Result<Vec<u8>> {
-        self.current_plugin()?.get_output_data()
+        let mut bytes = vec![];
+        self.current_plugin(|plugin| {
+            bytes = plugin.get_output_data()?;
+            Ok(())
+        })?;
+        Ok(bytes)
     }
     pub fn load_ui_window_layout(&mut self) -> Result<()> {
-        match self.current_plugin()?.load_ui_window_layout() {
+        let mut result = Ok(0);
+        self.current_plugin(|plugin| {
+            result = plugin.load_ui_window_layout();
+            Ok(())
+        })?;
+        match result {
             Ok(0) => Ok(()),
             Ok(_) => self.set_failure(),
             Err(err) => Err(err),
         }
     }
     pub fn get_ui_window_layout(&mut self) -> Result<Vec<u8>> {
-        self.current_plugin()?.get_ui_window_layout()
+        let mut bytes = vec![];
+        self.current_plugin(|plugin| {
+            bytes = plugin.get_ui_window_layout()?;
+            Ok(())
+        })?;
+        Ok(bytes)
     }
     pub fn set_ui_component_layout(
         &mut self,
@@ -538,10 +619,12 @@ impl ModelIOPluginController {
         data: &[u8],
         reload: &mut bool,
     ) -> Result<()> {
-        match self
-            .current_plugin()?
-            .set_ui_component_layout(id, data, reload)
-        {
+        let mut result = Ok(0);
+        self.current_plugin(|plugin| {
+            result = plugin.set_ui_component_layout(id, data, reload);
+            Ok(())
+        })?;
+        match result {
             Ok(0) => Ok(()),
             Ok(_) => self.set_failure(),
             Err(err) => Err(err),
@@ -557,31 +640,49 @@ impl ModelIOPluginController {
         self.failure_reason = Some(value.to_string());
     }
     pub fn destroy(&mut self) {
-        self.plugins.iter_mut().for_each(|plugin| plugin.destroy())
+        self.plugins
+            .lock()
+            .unwrap()
+            .iter_mut()
+            .for_each(|plugin| plugin.destroy())
     }
     pub fn terminate(&mut self) {
         self.plugins
+            .lock()
+            .unwrap()
             .iter_mut()
             .for_each(|plugin| plugin.terminate())
     }
     #[allow(unused)]
-    pub(super) fn all_plugins_mut(&mut self) -> &mut [ModelIOPlugin] {
-        &mut self.plugins
+    pub(super) fn all_plugins_mut(&mut self) -> Arc<Mutex<Vec<ModelIOPlugin>>> {
+        Arc::clone(&self.plugins)
     }
     fn set_failure(&mut self) -> Result<()> {
-        let value = self.current_plugin()?.failure_reason()?;
+        let mut value = String::new();
+        self.current_plugin(|plugin| {
+            value = plugin.failure_reason()?;
+            Ok(())
+        })?;
         if !value.is_empty() {
             self.failure_reason = Some(value);
         }
-        let value = self.current_plugin()?.recovery_suggestion()?;
+        let mut value = String::new();
+        self.current_plugin(|plugin| {
+            value = plugin.recovery_suggestion()?;
+            Ok(())
+        })?;
         if !value.is_empty() {
             self.recovery_suggestion = Some(value);
         }
         Ok(())
     }
-    fn current_plugin(&mut self) -> Result<&mut ModelIOPlugin> {
+    fn current_plugin<T>(&mut self, mut cb: T) -> Result<()>
+    where
+        T: FnMut(&mut ModelIOPlugin) -> Result<()>,
+    {
         if let Some(plugin_index) = self.plugin_index {
-            Ok(&mut self.plugins[plugin_index])
+            let plugins = &mut self.plugins.lock().unwrap();
+            cb(&mut plugins[plugin_index])
         } else {
             Err(anyhow::anyhow!("plugin is not set"))
         }
