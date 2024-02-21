7632267728db15566ca191356233d92a7cf9e804
GPTC <149788963+g-p-t-c@users.noreply.github.com>
2024-02-21 20:37:56 +0800

    Update DefaultTranslator.cc

--- emapp/src/DefaultTranslator.cc
+++ emapp/src/DefaultTranslator.cc
@@ -38,8 +38,8 @@ DefaultTranslator::loadFromMemory(const nanoem_u8_t *ptr, size_t length)
             const bool match =
                 (unit->language == NANOEM__COMMON__LANGUAGE__LC_JAPANESE && m_language == kLanguageTypeJapanese) ||
                 (unit->language == NANOEM__COMMON__LANGUAGE__LC_ENGLISH && m_language == kLanguageTypeEnglish) ||
-                (unit->language == NANOEM__COMMON__LANGUAGE__LC_SCHINESE && m_language == kLanguageTypeSChinese) ||
-                (unit->language == NANOEM__COMMON__LANGUAGE__LC_TCHINESE && m_language == kLanguageTypeTChinese);
+                (unit->language == NANOEM__COMMON__LANGUAGE__LC_SIMPLIFIED_CHINESE && m_language == kLanguageTypeSChinese) ||
+                (unit->language == NANOEM__COMMON__LANGUAGE__LC_TRADITIONAL_CHINESE && m_language == kLanguageTypeTChinese);
             if (match) {
                 for (size_t j = 0, numPhrases = unit->n_phrases; j < numPhrases; j++) {
                     const Nanoem__Translation__Phrase *phrase = unit->phrases[j];
