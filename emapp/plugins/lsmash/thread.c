f3aa81dee40ff8f2dfd8600e5846bd96170c5bde
hkrn <129939+hkrn@users.noreply.github.com>
2025-01-02 15:30:45 +0900

    [plugin] fix build of `plugin_lsmash`

--- emapp/plugins/lsmash/thread.c
+++ emapp/plugins/lsmash/thread.c
@@ -2,6 +2,8 @@
 #if !defined(_WIN32)
 #include <errno.h>
 #include <stdint.h>
+#include <string.h> /* for memset */
 #include <strings.h>
+#include <pthread.h> /* for pthread_setname_np */
 #endif
 #include "thread.h"
